9c8266b233f25db2254cbc2d44ceb33c2b552832
Ammadrew <62050122@kmitl.ac.th>
2022-07-27 21:36:16 +0700

    add file

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
-import './style.scss'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.scss';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const pokemonListElement = document.getElementById('pokemon-list');
+if(pokemonListElement != null) {
+  const pokemonListUrl = ` http://localhost:3000/data`;
 
-setupCounter(document.querySelector('#counter'))
+  fetch(pokemonListUrl, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      let pokemons = data;
+
+      pokemons.map((p) => {
+        let cardType = p.type.map(t => {
+          return `<div class="type ${t}">${t}</div>`
+        }).join('')
+        let itemElementText = `
+        <div class="card">
+          <div class="thumbnail-bg">
+            <img src="${p.ThumbnailImage}" alt="${p.name}" class="image"/>
+          </div>
+          <div>
+            <div class="number">#${p.number}</div>
+            <div class="name">${p.name}</div>
+            <div class="group-type">${cardType}</div>
+          </div>
+        </div>
+        `
+        pokemonListElement.insertAdjacentHTML('beforeend', itemElementText)
+      })
+    })
+}
